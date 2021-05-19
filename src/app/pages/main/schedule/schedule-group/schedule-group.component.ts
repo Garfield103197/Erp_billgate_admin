ff514c27eec3d6dc23a8b620056d6df811dc30ac
hai.cd <hai.cd194@gmail.com>
2021-05-19 09:18:00 +0700

    router schedule

--- src/app/pages/main/schedule/schedule-group/schedule-group.component.ts
+++ src/app/pages/main/schedule/schedule-group/schedule-group.component.ts
@@ -1,93 +1,92 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-schedule-group',
-  templateUrl: './schedule-group.component.html',
-  styleUrls: ['./schedule-group.component.scss']
+    selector: 'app-schedule-group',
+    templateUrl: './schedule-group.component.html',
+    styleUrls: ['./schedule-group.component.scss']
 })
 export class ScheduleGroupComponent implements OnInit {
 
-  constructor() { }
-  list1 = [
-    {
-      header: 'Mầm non',
-      group: '3 lớp',
-      member: '30 thành viên'
-    },
-  ]
 
-  list2 = [
-    {
-      header: 'Khối 1',
-      group: '10 lớp',
-      member: '140 thành viên'
-    },
-    {
-      header: 'Khối 2',
-      group: '10 lớp',
-      member: '140 thành viên'
-    },
-    {
-      header: 'Khối 3',
-      group: '10 lớp',
-      member: '140 thành viên'
-    },
-    {
-      header: 'Khối 4',
-      group: '10 lớp',
-      member: '140 thành viên'
-    },
-    {
-      header: 'Khối 5',
-      group: '10 lớp',
-      member: '140 thành viên'
-    },
-  ]
+    list1 = [
+        {
+            header: 'Mầm non',
+            group: '3 lớp',
+            member: '30 thành viên'
+        },
+    ]
 
-  list3 = [
-    {
-      header: 'Khối 6 ',
-      group: '10 lớp',
-      member: '300 thành viên'
-    },
-    {
-      header: 'Khối 7',
-      group: '10 lớp',
-      member: '300 thành viên'
-    },
-    {
-      header: 'Khối 8',
-      group: '10 lớp',
-      member: '300 thành viên'
-    },
-    {
-      header: 'Khối 9',
-      group: '10 lớp',
-      member: '300 thành viên'
-    },
-  ]
+    list2 = [
+        {
+            header: 'Khối 1',
+            group: '10 lớp',
+            member: '140 thành viên'
+        },
+        {
+            header: 'Khối 2',
+            group: '10 lớp',
+            member: '140 thành viên'
+        },
+        {
+            header: 'Khối 3',
+            group: '10 lớp',
+            member: '140 thành viên'
+        },
+        {
+            header: 'Khối 4',
+            group: '10 lớp',
+            member: '140 thành viên'
+        },
+        {
+            header: 'Khối 5',
+            group: '10 lớp',
+            member: '140 thành viên'
+        },
+    ]
 
-highSchool = [
-  {
-    header: 'Khối 10',
-    group: '10 lớp',
-    member: '300 thành viên'
-  },
-  {
-    header: 'Khối 11',
-    group: '10 lớp',
-    member: '300 thành viên'
-  },
-  {
-    header: 'Khối 12',
-    group: '10 lớp',
-    member: '300 thành viên'
-  },
-]
+    list3 = [
+        {
+            header: 'Khối 6 ',
+            group: '10 lớp',
+            member: '300 thành viên'
+        },
+        {
+            header: 'Khối 7',
+            group: '10 lớp',
+            member: '300 thành viên'
+        },
+        {
+            header: 'Khối 8',
+            group: '10 lớp',
+            member: '300 thành viên'
+        },
+        {
+            header: 'Khối 9',
+            group: '10 lớp',
+            member: '300 thành viên'
+        },
+    ]
 
-  ngOnInit(): void {
-    console.log(13123123);
+    highSchool = [
+        {
+            header: 'Khối 10',
+            group: '10 lớp',
+            member: '300 thành viên'
+        },
+        {
+            header: 'Khối 11',
+            group: '10 lớp',
+            member: '300 thành viên'
+        },
+        {
+            header: 'Khối 12',
+            group: '10 lớp',
+            member: '300 thành viên'
+        },
+    ]
+    constructor() { }
+    ngOnInit(): void {
 
-  }
+    }
 
 }
--- src/app/pages/main/schedule/schedule.module.ts
+++ src/app/pages/main/schedule/schedule.module.ts
@@ -1,20 +1,21 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { BaseCardModule } from 'src/app/components/card/base-card/base-card.component';
-import { scheduleRoute } from './schedule.routes';
-import { ScheduleGroupComponent } from './schedule-group/schedule-group.component';
 import { ScheduleGroupDetailComponent } from './schedule-group-detail/schedule-group-detail.component';
+import { ScheduleGroupComponent } from './schedule-group/schedule-group.component';
 import { ScheduleListComponent } from './schedule-list/schedule-list.component';
+import { ScheduleComponent } from './schedule.component';
+import { scheduleRoute } from './schedule.routes';
 
 
 @NgModule({
-    declarations: [ScheduleGroupComponent, ScheduleGroupDetailComponent, ScheduleListComponent],
+    declarations: [ScheduleComponent, ScheduleGroupDetailComponent, ScheduleListComponent, ScheduleGroupComponent],
     imports: [
         CommonModule,
         RouterModule.forChild(scheduleRoute),
         BaseCardModule
     ],
-    exports: [ScheduleGroupComponent]
+    exports: [ScheduleComponent]
 })
 export class ScheduleModule { }
